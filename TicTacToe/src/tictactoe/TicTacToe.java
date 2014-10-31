9ec10369c8db9c7468fc10d5984ac576c48d42d9
Md. Mahadi Hasan Nahid <nahid@student.sust.edu>
2014-10-30 22:24:00 -0700

    Project Initialization

---
+++ TicTacToe/src/tictactoe/TicTacToe.java
@@ -0,0 +1,16 @@
+package tictactoe;
+
+/**
+ *
+ * @author MdMahadiHasan
+ */
+public class TicTacToe {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
